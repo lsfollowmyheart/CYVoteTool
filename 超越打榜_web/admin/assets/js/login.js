159de029bf9d380f70b7ebbe1ec17dad277d564e
Shuo Tian <33530007+BurgerBurglar@users.noreply.github.com>
2019-03-14 10:39:01 -0500

    Add files via upload

---
+++ 超越打榜_web/admin/assets/js/login.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $("#login").click(function(){
+        var username = $("#user-name").val();
+        var password = $("#user-password").val();
+        location.href="index.html";
+        // $.ajax({
+        //    // url:"dizhi",
+        //   //  data:{"参数":shuzhi},
+        //     type:post,
+        //     success:function(){
+        //         
+        //     }
+        // });
+        // if(username ==""){
+        //     $("#user-name").parent().addClass("am-form-error");
+        // }else{
+
+        // }
+    });
+});
